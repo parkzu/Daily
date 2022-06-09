ccf22fe0b972b64334fc85dc806fb798cec65d13
Jiyu <51067494+parkzu@users.noreply.github.com>
2022-06-09 23:11:40 +0900

    컴포넌트 사용하기

--- VSCode/React/ArrayRendering/UserList.js
+++ VSCode/React/ArrayRendering/UserList.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+function User({ user }) {
+  return (
+    <div>
+      <b>{user.username}</b> <span>({user.email})</span>
+    </div>
+  );
+}
+
 function UserList() {
   const users = [
     {
@@ -18,17 +26,12 @@ function UserList() {
       email: 'liz@example.com'
     }
   ];
+
   return (
     <div>
-      <div>
-        <b>{users[0].username}</b> <span>({users[0].email})</span>
-      </div>
-      <div>
-        <b>{users[1].username}</b> <span>({users[1].email})</span>
-      </div>
-      <div>
-        <b>{users[2].username}</b> <span>({users[1].email})</span>
-      </div>
+      <User user={users[0]} />
+      <User user={users[1]} />
+      <User user={users[2]} />
     </div>
   );
 }
