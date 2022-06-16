486e21032cdc09bfafe414dfce5fb9866e67b12d
Jiyu <51067494+parkzu@users.noreply.github.com>
2022-06-16 21:45:12 +0900

    버튼을 클릭했을 때 Input창이 보이고 다시 누르면 안보이게하기

---
+++ SandBox/React/HookFlow.js
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <body>
+    <div id="root"></div>
+    <script src="https://unpkg.com/react@17/umd/react.development.js"></script>
+    <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"></script>
+    <script src=" https://unpkg.com/@babel/standalone/babel.min.js"></script>
+    <script type="text/babel">
+      const rootElement = document.getElementById("root");
+
+      const App = () => {
+        const [show, setShow] = React.useState(false);
+
+        function handleClick() {
+          setShow(true);
+          if (show) {
+            setShow(false);
+          } else setShow(true);
+        }
+
+        return (
+          <>
+            <button onClick={handleClick}>Search</button>
+            {show ? (
+              <>
+                <input />
+                <p></p>
+              </>
+            ) : null}
+          </>
+        );
+      };
+
+      ReactDOM.render(<App />, rootElement);
+    </script>
+  </body>
+</html>
