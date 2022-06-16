d1a4003b66f621e3f897c99f0c9b865738db67e3
Jiyu <51067494+parkzu@users.noreply.github.com>
2022-06-16 21:57:01 +0900

    Console log 를 사용하여 동작 여부 확인하기

--- SandBox/React/HookFlow.js
+++ SandBox/React/HookFlow.js
@@ -9,10 +9,24 @@
       const rootElement = document.getElementById("root");
 
       const App = () => {
-        const [show, setShow] = React.useState(false);
+        console.log("APP render start");
+        const [show, setShow] = React.useState(() => {
+          console.log("APP useState");
+          return false;
+        });
+
+        React.useEffect(() => {
+          console.log("APP useEffect, no deps");
+        });
+        React.useEffect(() => {
+          console.log("APP useEffect, empty deps");
+        }, []);
+        React.useEffect(() => {
+          console.log("APP useEffect, [show]");
+        }, [show]);
 
         function handleClick() {
-          setShow((prev) => !prev); //입력받은 값의 반대로 변환
+          setShow((prev) => !prev);
         }
 
         return (
@@ -28,6 +42,7 @@
         );
       };
 
+      console.log("APP render end");
       ReactDOM.render(<App />, rootElement);
     </script>
   </body>
