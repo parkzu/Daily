85dc154d372c61a0d2890092e14a40b9ed519e9b
Jiyu <51067494+parkzu@users.noreply.github.com>
2022-06-27 19:25:14 +0900

    style을 사용하여 일정시간 경과후 색상 변경 확인하기

--- SandBox/React/useRef.js
+++ SandBox/React/useRef.js
@@ -9,13 +9,22 @@
       const rootElement = document.getElementById("root");
       const App = () => {
         const inputRef = React.useRef();
+        const divRef = React.useRef();
         React.useEffect(() => {
           inputRef.current.focus();
           // document.getElementById("input").focus();
+
+          setTimeout(() => {
+            divRef.current.style.backgroundColor = "pink";
+          }, 1000);
         }, []);
         return (
           <>
             <input ref={inputRef} />
+            <div
+              ref={divRef}
+              style={{ height: 100, width: 300, backgroundColor: "brown" }}
+            />
           </>
         );
       };
