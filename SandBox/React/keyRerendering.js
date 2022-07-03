aea3f7bfa8d80ee79b37b6bf8c58be7c5c0c4884
Jiyu <51067494+parkzu@users.noreply.github.com>
2022-07-03 19:34:00 +0900

    key value를 설정하여 클릭속성 추가하기

---
+++ SandBox/React/keyRerendering.js
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+  <body>
+    <div id="root"></div>
+    <script src="https://unpkg.com/react@17/umd/react.development.js"></script>
+    <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"></script>
+    <script src=" https://unpkg.com/@babel/standalone/babel.min.js"></script>
+    <script type="text/babel">
+      const rootElement = document.getElementById("root");
+
+      const todos = [
+        { id: 1, value: "Wash dishies" },
+        { id: 2, value: "Clean the bed" },
+        { id: 3, value: "Running" },
+        { id: 4, value: "Learning" }
+      ];
+
+      const App = () => {
+        const [items, setItems] = React.useState(todos);
+
+        const handleDoneClick = (todo) => {
+          setItems((items) => items.filter((item) => item !== todo));
+        };
+
+        return (
+          <>
+            {items.map((todo) => (
+              <div>
+                <span>{todo.value}</span>
+                <button onClick={() => handleDoneClick(todo)}>Done</button>
+              </div>
+            ))}
+          </>
+        );
+      };
+
+      ReactDOM.render(<App />, rootElement);
+    </script>
+  </body>
+</html>
