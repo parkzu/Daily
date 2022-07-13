3ac8a4a6135555ce22a43ed84d1b4dc9f9df7443
Jiyu <51067494+parkzu@users.noreply.github.com>
2022-07-13 22:33:16 +0900

    Terminal 연결하기

---
+++ VSCode/React/Official Documentation/Props/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Composition from './components/2-4.Props/Composition';
+
+function App() {
+  return (
+    <div className="App">
+      <Composition />
+    </div>
+  );
+}
+
+export default App;
