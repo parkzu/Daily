c871127c7450fba3dd783a61ed2eea949c61f466
Jiyu <51067494+parkzu@users.noreply.github.com>
2022-07-15 23:03:08 +0900

    FunctionalComponent 불러오기

--- VSCode/React/Official Documentation/State/App.js
+++ VSCode/React/Official Documentation/State/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import Extratction from './components/2-4.Props/Extraction/Extraction';
 import Composition from './components/2-4.Props/Composition';
 import ClassComponent from './components/2-5.State/ClassComponent';
+import FunctionalComponent from './components/2-5.State/FunctionalComponent';
 
 function App() {
   return (
     <div className="App">
+      <FunctionalComponent />
       <ClassComponent />
       <Extratction />
       <Composition />
