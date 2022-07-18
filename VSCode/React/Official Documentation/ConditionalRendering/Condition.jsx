f898f14aefa930c58d286119d9cfc2c99054661b
Jiyu <51067494+parkzu@users.noreply.github.com>
2022-07-18 23:45:38 +0900

    0을 처리하기 위한 조건
    1. Boolean으로 감싸기
    2. 삼항연산자 사용하기

--- VSCode/React/Official Documentation/ConditionalRendering/Condition.jsx
+++ VSCode/React/Official Documentation/ConditionalRendering/Condition.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function UserGreeting(props) {
-    return <h1>{props.name && `${props.name},`} Welcome {props.count && `It's ${props.count} times`}</h1>
+    return <h1>{props.name && `${props.name},`} Welcome {props.count ? `It's ${props.count} times` : null}</h1>
 }
 
 function GuestGreeting(props) {
@@ -9,10 +9,6 @@ function GuestGreeting(props) {
 }
 
 function Greeting (props) {
-    // if(props.isLoggedIn){
-    //     return <UserGreeting name="jiyu" count={0} />
-    // }
-    // return <GuestGreeting />;
     return props.isLoggedIn ? <UserGreeting name="jiyu" count={0} /> : <GuestGreeting />;
 }
 export default function Condition() {
