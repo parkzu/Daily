8250410571fe18fef3426932863cb803e8c62b47
Jiyu <51067494+parkzu@users.noreply.github.com>
2022-07-18 23:43:22 +0900

    count가 0일 때 false가 나오는 것 확인하기

--- VSCode/React/Official Documentation/ConditionalRendering/Condition.jsx
+++ VSCode/React/Official Documentation/ConditionalRendering/Condition.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function UserGreeting(props) {
-    return <h1>{props.name && `${props.name},`} Welcome It's {props.count} times</h1>
+    return <h1>{props.name && `${props.name},`} Welcome {props.count && `It's ${props.count} times`}</h1>
 }
 
 function GuestGreeting(props) {
@@ -9,6 +9,10 @@ function GuestGreeting(props) {
 }
 
 function Greeting (props) {
+    // if(props.isLoggedIn){
+    //     return <UserGreeting name="jiyu" count={0} />
+    // }
+    // return <GuestGreeting />;
     return props.isLoggedIn ? <UserGreeting name="jiyu" count={0} /> : <GuestGreeting />;
 }
 export default function Condition() {
