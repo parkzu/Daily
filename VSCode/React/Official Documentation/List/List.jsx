344600130a8e94f9f0ede412ab225655396f9d0c
Jiyu <51067494+parkzu@users.noreply.github.com>
2022-07-19 21:35:11 +0900

    todoList 배열을 사용하여 List 만들기

--- VSCode/React/Official Documentation/List/List.jsx
+++ VSCode/React/Official Documentation/List/List.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 
 export default function List() {
-    const numbers = [1,2,3,4,5];
-  return (
-    <div>
-        {numbers.map((item) => (
-        <li key={item.toString()}>{item}</li>
-        ))}
-    </div>
-  )
+//     const numbers = [1,2,3,4,5];
+//   return (
+//     <div>
+//         {numbers.map((item) => (
+//         <li key={item.toString()}>{item}</li>
+//         ))}
+//     </div>
+//   )
+    const todos = [
+        {id: 1, text: 'Drink Water'},
+        {id: 2, text: 'Wash Car'},
+        {id: 3, text: 'Listen Lecture'},
+        {id: 4, text: 'Go to bed'}
+    ];
+
+    const Item = (props) => {
+        return <li>{props.text}</li>;
+    }
+
+    const todoList = todos.map((todo) => <Item{...todo} />)
+
+    return (
+        <>{todoList}</>
+    )
 }
