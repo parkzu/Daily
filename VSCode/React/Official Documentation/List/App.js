addd223f301a84de88f058f8677bfead6d198970
Jiyu <51067494+parkzu@users.noreply.github.com>
2022-07-19 21:38:50 +0900

    List.jsx 불러오기

---
+++ VSCode/React/Official Documentation/List/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import List from './components/2-9.List/List';
+// import Condition from './components/2-8.ConditionalRendering/Condition';
+// import Event from './components/2-7.Event/Event';
+// import ClassComponent2 from './components/2-6.LifeCycle/ClassComponent';
+// import Extratction from './components/2-4.Props/Extraction/Extraction';
+// import Composition from './components/2-4.Props/Composition';
+// import ClassComponent from './components/2-5.State/ClassComponent';
+// import FunctionalComponent from './components/2-5.State/FunctionalComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <List />
+      {/* <Condition /> */}
+      {/* <Event /> */}
+      {/* <ClassComponent2 /> */}
+      {/* <FunctionalComponent />
+      <ClassComponent />
+      <Extratction />
+      <Composition /> */}
+    </div>
+  );
+}
+
+export default App;
