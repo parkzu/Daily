795e60abb374183d3b620a6f28cff785368d3d9e
Jiyu <51067494+parkzu@users.noreply.github.com>
2022-07-20 22:48:45 +0900

    입력칸과 submit 버튼을 가지고 있는 form 생성하기

---
+++ VSCode/React/Official Documentation/Form/ControlledComponent.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+export default function ControlledComponent() {
+    
+    const  [value,setValue] = useState('');
+
+    function handleChange(event) {
+        setValue(event.target.value);
+      }
+    
+    function handleSubmit(event) {
+        alert('A name was submitted: ' + value);
+        event.preventDefault();
+      }
+    
+
+  return (
+    <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={value} onChange={handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+  )
+}
