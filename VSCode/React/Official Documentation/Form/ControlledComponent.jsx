9b946399f5ee8f24d3277a9aa565ce64575607d6
Jiyu <51067494+parkzu@users.noreply.github.com>
2022-07-21 22:47:40 +0900

    Form style추가하기

--- VSCode/React/Official Documentation/Form/ControlledComponent.jsx
+++ VSCode/React/Official Documentation/Form/ControlledComponent.jsx
@@ -2,10 +2,17 @@ import React, { useState } from 'react'
 
 export default function ControlledComponent() {
     
-    const  [value,setValue] = useState('');
+    const  [value,setValue] = useState("");
+    const  [essay,setEssay] = useState(
+      "Please write an essay about your favorite DOM element."
+      );
 
     function handleChange(event) {
         setValue(event.target.value);
+      }
+    
+    function handleEssayChange(event) {
+        setEssay(event.target.value);
       }
     
     function handleSubmit(event) {
@@ -19,6 +26,12 @@ export default function ControlledComponent() {
         <label>
           Name:
           <input type="text" value={value} onChange={handleChange} />
+        </label>
+        <br />
+        <br />
+        <label>
+          Essay:
+          <textarea value={essay} onChange={handleEssayChange} />
         </label>
         <input type="submit" value="Submit" />
       </form>
