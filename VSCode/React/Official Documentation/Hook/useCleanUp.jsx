111397bf508cd9a54255a058fb7c726094426f21
Jiyu <51067494+parkzu@users.noreply.github.com>
2022-07-25 20:34:11 +0900

    Hook을 사용했을 경우와 차이 확인하기

--- VSCode/React/Official Documentation/Hook/useCleanUp.jsx
+++ VSCode/React/Official Documentation/Hook/useCleanUp.jsx
@@ -1,26 +1,18 @@
-class Example extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0
-    };
-  }
+import React, { useState, useEffect } from 'react';
 
-  componentDidMount() {
-    document.title = `You clicked ${this.state.count} times`;
-  }
-  componentDidUpdate() {
-    document.title = `You clicked ${this.state.count} times`;
-  }
+function Example() {
+  const [count, setCount] = useState(0);
 
-  render() {
-    return (
-      <div>
-        <p>You clicked {this.state.count} times</p>
-        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
-          Click me
-        </button>
-      </div>
-    );
-  }
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
 }
