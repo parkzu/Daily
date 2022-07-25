819e7677a9e93b494bd9a9d25435e6baed2f1089
Jiyu <51067494+parkzu@users.noreply.github.com>
2022-07-25 20:37:25 +0900

    Hook을 사용했을 때 cleanUp이 필요한 경우

---
+++ VSCode/React/Official Documentation/Hook/useCleanUp3.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+
+function FriendStatus(props) {
+  const [isOnline, setIsOnline] = useState(null);
+
+  useEffect(() => {
+    function handleStatusChange(status) {
+      setIsOnline(status.isOnline);
+    }
+    ChatAPI.subscribeToFriendStatus(props.friend.id, handleStatusChange);
+    // effect 이후에 어떻게 정리(clean-up)할 것인지 표시합니다.
+    return function cleanup() {
+      ChatAPI.unsubscribeFromFriendStatus(props.friend.id, handleStatusChange);
+    };
+  });
+
+  if (isOnline === null) {
+    return 'Loading...';
+  }
+  return isOnline ? 'Online' : 'Offline';
+}
