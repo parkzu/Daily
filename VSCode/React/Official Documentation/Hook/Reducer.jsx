18376e2399faa517deadaa3a6351a199f7a3cbf4
Jiyu <51067494+parkzu@users.noreply.github.com>
2022-07-28 19:33:39 +0900

    dispatch를 사용했을 경우 button값에 따른 name 변화
    여러 상태를 관리하기 편리함

--- VSCode/React/Official Documentation/Hook/Reducer.jsx
+++ VSCode/React/Official Documentation/Hook/Reducer.jsx
@@ -1,16 +1,16 @@
 import React, { useReducer } from 'react'
 
 export default function Reducer() {
-    const initialState = {count: 0};
+    const initialState = {count: 0, name: 'jimmy'};
     
     function reducer(state, action) {
         switch(action.type) {
             case 'reset':
                 return initialState;
             case 'increment':
-                return { count: state.count + 1};
+                return { count: state.count + 1, name: 'timmy'};
             case 'decrement':
-                return { count: state.count - 1};
+                return { count: state.count - 1, name: 'amy'};
             default:
                 throw new Error();
         }
@@ -19,7 +19,8 @@ export default function Reducer() {
     const [state, dispatch] = useReducer(reducer, initialState);
     return (
     <div>
-            Count(Reducer): {state.count}
+            Count(Reducer): {state.count}/
+            Name: {state.name}
             <button onClick={() => dispatch({type: 'reset'})}>Reset</button>
             <button onClick={() => dispatch({type: 'decrement'})}>-</button>
             <button onClick={() => dispatch({type: 'increment'})}>+</button>
