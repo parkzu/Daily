0f9bdb5ecb7699be83e389422b9c77827ff8df3b
Jiyu <51067494+parkzu@users.noreply.github.com>
2022-07-28 19:31:11 +0900

    State.jsx와 Reducer.jsx 불러오기

---
+++ VSCode/React/Official Documentation/Hook/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import State from './components/3-3.Hooks/State';
+import Reducer from './components/3-3.Hooks/Reducer';
+
+function App() {
+  return (
+    <div className="App">
+      <Reducer />
+      <State />
+    </div>
+  );
+}
+
+export default App;
