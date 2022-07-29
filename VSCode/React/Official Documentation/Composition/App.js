ac5e89ce2ae42abb34e951d8de5cd6cbaf6fc7b9
Jiyu <51067494+parkzu@users.noreply.github.com>
2022-07-29 23:06:33 +0900

    WelcomeDialog.jsx 불러오기

---
+++ VSCode/React/Official Documentation/Composition/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import WelcomeDialog from './components/3-5.Composition/WelcomeDialog';
+
+function App() {
+  return (
+    <div className="App">
+      <WelcomeDialog />
+    </div>
+  );
+}
+
+export default App;
