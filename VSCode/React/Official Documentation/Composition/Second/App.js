cea74d43df70f3df0095164c4a6cd22244aaccf3
Jiyu <51067494+parkzu@users.noreply.github.com>
2022-07-30 22:10:10 +0900

    Dialog.jsx 불러오기

---
+++ VSCode/React/Official Documentation/Composition/Second/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Dialog from './components/3-6.Composition2/Dialog';
+
+function App() {
+  return (
+    <div className="App">
+      <Dialog />
+
+    </div>
+  );
+}
+
+export default App;
