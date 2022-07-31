311b6e1e8e8b277c6d029a3cd941d0d08980d4b9
Jiyu <51067494+parkzu@users.noreply.github.com>
2022-07-31 23:36:14 +0900

    style 추가하기

--- VSCode/React/Official Documentation/Composition/Second/ThankyouDialog.jsx
+++ VSCode/React/Official Documentation/Composition/Second/ThankyouDialog.jsx
@@ -3,8 +3,10 @@ import Dialog from './Dialog'
 
 export default function ThankyouDialog() {
   return (
-    <Dialog title="Thanks" description="It is honor to meet you!" 
-    button="close" />
+    <Dialog 
+    title={<h1 style = {{color: 'purple' }}>Thanks</h1>}
+    description="It is honor to meet you!" 
+    button={<button style={{backgroundColor: "blue", color: "white"}}>close</button>} />
 
   )
 }
