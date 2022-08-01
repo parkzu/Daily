58f0dbecc4bb2ca641118863fe2554bd9448ebe6
Jiyu <51067494+parkzu@users.noreply.github.com>
2022-08-01 22:59:25 +0900

    Input.jsx와 Button.jsx 불러오기

---
+++ VSCode/React/Official Documentation/HOC/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Button from './components/3-7.HOC/Button';
+import Input from './components/3-7.HOC/Button copy';
+
+function App() {
+  return (
+    <div className="App">
+      <Input />
+      <br />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
