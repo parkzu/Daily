42064ce72e85ce038fd9c6da9075a35a720a03b5
Jiyu <51067494+parkzu@users.noreply.github.com>
2022-08-10 23:57:33 +0900

    useCallback 을 사용하여 handleChange함수 간결히 불러오기

--- VSCode/React/Official Documentation/Memoization/Comments.jsx
+++ VSCode/React/Official Documentation/Memoization/Comments.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import CommentItem from "./CommentItem"
 
 export default function Comments({commentList}) {
+  const handleChange = useCallback(() => {
+      console.log("눌림");
+  }, []);
   return (
     <div>
         {commentList.map((comment) => (
@@ -10,7 +13,7 @@ export default function Comments({commentList}) {
             title={comment.title}
             content={comment.content}
             likes={comment.likes}
-            onClick={() => console.log('눌림')}
+            onClick={handleChange}
             />
         ))}
     </div>
