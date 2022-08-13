3a7950815e2ff89bfe70943b89faf7aa2183a807
Jiyu <51067494+parkzu@users.noreply.github.com>
2022-08-13 22:46:38 +0900

    상태를 변화시키는 함수를 생성하여 onClick에 적용하기

--- VSCode/React/Official Documentation/Context/Example.jsx
+++ VSCode/React/Official Documentation/Context/Example.jsx
@@ -7,13 +7,29 @@ export default class Example extends Component {
         super(props);
         this.state = {
             theme: themes.light,
+        };
+        this.toggleTheme = () => {
+            this.setState((prev) => ({
+                theme: prev.theme === themes.dark ? themes.light : themes.dark,
+            }));
         }
     }
   render() {
     return (
       <div>
         <ThemeContext.Provider value={this.state.theme}>
-            <ThemedButton />
+            <ThemedButton changeTheme={this.toggleTheme} />
+            <ThemeContext.Consumer>
+                {theme => (
+                <div 
+                    style={{
+                        height: 300, 
+                        width: 300, 
+                        backgroundColor: theme.background,
+                    }}>
+                </div>
+                )}
+            </ThemeContext.Consumer>
         </ThemeContext.Provider>
         <ThemedButton />
       </div>
