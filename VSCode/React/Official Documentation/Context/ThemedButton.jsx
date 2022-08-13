5e8cd090cd4ae7e4e61261f2d13a66a2c8431863
Jiyu <51067494+parkzu@users.noreply.github.com>
2022-08-13 22:39:26 +0900

    default 값을 가진 button 생성하기

---
+++ VSCode/React/Official Documentation/Context/ThemedButton.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { ThemeContext } from './ThemeContext';
+
+class ThemedButton extends Component {
+  render() {
+    let props = this.props;
+    let theme = this.context;
+    return (
+      <button{...props} 
+      style={{backgroundColor: theme.background, color: theme.foreground}}
+      >Button</button>
+    );
+  }
+}
+
+ThemedButton.contextType = ThemeContext;
+
+export default ThemedButton;
