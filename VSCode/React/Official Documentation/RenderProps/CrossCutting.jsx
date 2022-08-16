bffdc59fec9b74f5487a343cc937b56f810bd3d5
Jiyu <51067494+parkzu@users.noreply.github.com>
2022-08-16 21:29:20 +0900

    Mouse 함수를 재정의 하여 캡슐화하기

--- VSCode/React/Official Documentation/RenderProps/CrossCutting.jsx
+++ VSCode/React/Official Documentation/RenderProps/CrossCutting.jsx
@@ -1,4 +1,5 @@
-class MouseTracker extends React.Component {
+// <Mouse> 컴포넌트는 우리가 원하는 행위를 캡슐화 합니다...
+class Mouse extends React.Component {
   constructor(props) {
     super(props);
     this.handleMouseMove = this.handleMouseMove.bind(this);
@@ -15,9 +16,21 @@ class MouseTracker extends React.Component {
   render() {
     return (
       <div style={{ height: '100vh' }} onMouseMove={this.handleMouseMove}>
-        <h1>Move the mouse around!</h1>
+
+        {/* ...하지만 <p>가 아닌 다른것을 렌더링하려면 어떻게 해야 할까요? */}
         <p>The current mouse position is ({this.state.x}, {this.state.y})</p>
       </div>
+    );
+  }
+}
+
+class MouseTracker extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Move the mouse around!</h1>
+        <Mouse />
+      </>
     );
   }
 }
