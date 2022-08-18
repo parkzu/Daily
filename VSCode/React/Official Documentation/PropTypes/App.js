d02a1c137798c8c54c8d3cdcc522c0ea2700859b
Jiyu <51067494+parkzu@users.noreply.github.com>
2022-08-18 23:06:52 +0900

    Component.jsx 불러오기

---
+++ VSCode/React/Official Documentation/PropTypes/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Component from './components/3-14.PropTypes/Component';
+
+function App() {
+  return (
+    <div className="App">
+      <Component />
+    </div>
+  );
+}
+
+export default App;
